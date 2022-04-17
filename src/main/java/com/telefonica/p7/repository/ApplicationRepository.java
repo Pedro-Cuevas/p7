e380dc9cadadb7938645b126d25fb6982e7da87c
Pedro-Cuevas <prcuevasolarte@gmail.com>
2022-04-17 19:07:13 +0200

    error en query solucionado

--- src/main/java/com/telefonica/p7/repository/ApplicationRepository.java
+++ src/main/java/com/telefonica/p7/repository/ApplicationRepository.java
@@ -6,6 +6,6 @@ import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.repository.CrudRepository;
 
 public interface ApplicationRepository  extends CrudRepository<Application,String> {
-    @Query("SELECT * FROM APPLICATIONS WHERE OFFER.ID= :id")
+    @Query("SELECT * FROM APPLICATIONS WHERE APPLICATIONS.ID= :id")
     public Iterable<Application> getApplicationByID(String id);
 } 
